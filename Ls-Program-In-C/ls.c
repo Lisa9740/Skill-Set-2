2af217a94b518c6feb9fc5a37025b399c1903083
Lisa9740 <barretalison@gmail.com>
2023-08-17 19:54:54 +0200

    Fixed program by adding command's arguments

--- Ls-Program-In-C/ls.c
+++ Ls-Program-In-C/ls.c
@@ -2,24 +2,73 @@
 
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+
+int *optArg(char **opt, char **path);
+int showDirectory (DIR *directory);
+void showAllWithHiddenFiles (DIR *directory);
+
+int main(int argc, char* argv[])
 {
-    struct dirent *dir;
+    DIR *directory;
 
-    DIR *d;
-    d = opendir(".");
-
-    if (d == NULL)
-    {
-	printf("Could not open current directory");
-	return 0;
+     switch (argc) {
+	case 1:
+	showDirectory(opendir("."));
+	break;
+	case 2:
+        showDirectory(opendir(argv[1]));
+	break;
+	case 3:
+	*optArg(&argv[1], &argv[2]);
+	break;
+	default:
+	printf("Too much argument used. ls [option] [path]");
+	break;
     }
 
-     while ((dir = readdir(d)) != NULL)
-    	printf("%s\n", dir->d_name);
-     	closedir(d);
-
      return 0;
+
+}
+
+
+int showDirectory(DIR* directory){
+   struct dirent *entry;
+
+   if (directory == NULL) {
+	printf("Could not open current directory");
+   }
+
+   while ((entry = readdir(directory)) != NULL)
+       if (entry->d_name[0] != '.'
+	&& strcmp(entry->d_name, ".") != 0
+	  && strcmp(entry->d_name, "..") != 0)
+	{
+		printf("%s\n", entry->d_name);
+	}
+	closedir(directory);
+
+}
+
+int *optArg(char** opt, char** path)
+{
+
+  DIR *directory = opendir(*path);
+  struct dirent *entry;
+
+  if (strcmp(*opt,"-a") == 0) {
+	showAllWithHiddenFiles(directory);
+  }
+
+}
+
+void showAllWithHiddenFiles(DIR *directory)
+{
+  struct dirent *entry;
+
+  while ((entry = readdir(directory)))
+	printf("%s\n", entry->d_name);
+	closedir(directory);
 
 }
